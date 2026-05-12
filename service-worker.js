b853a8bfcca3e089f6974ee44a22e9b17e4eafeb
Duba Dida orobo <duba.519917@haramaya.edu.et>
2026-05-12 23:54:39 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hellobuyer-v4';
+const CACHE_NAME = 'hellobuyer-v5';
 const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
