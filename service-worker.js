0c94cfcab45b81d524fc5808a4dce4b1121e13d1
Duba Dida orobo <duba.519917@haramaya.edu.et>
2026-05-13 00:12:51 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,32 @@
-const CACHE_NAME = 'hellobuyer-v5';
-const urlsToCache = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'hellobuyer-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)).then(() => self.skipWaiting())
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
   );
 });
